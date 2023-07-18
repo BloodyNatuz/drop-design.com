e0fd320f6050f60f3226c7c8c35172645ae208ea
Nathuz <nathuznoepupin@gmail.com>
2023-07-18 17:14:03 +0200

    🚧 SpanColorAleat function

--- app/page.js
+++ app/page.js
@@ -1,5 +1,30 @@
+// Components
+import SpanAleatColor from './components/client/baselineColorAleat'
+
+// Custom Style
 import main from './style/main.scss'
 
+// Custom Font
+import { Roboto } from 'next/font/google'
+import { Montserrat } from 'next/font/google'
+ 
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+const montserrat = Montserrat({
+  weight: '900',
+  subsets: ['latin'],
+  display: 'swap',
+})
+
 export default function Landing() {
-  return <h1>Hello, Next.js!</h1>
+  return (
+    <main className='landing-page'>
+      <h1 className={montserrat.className}>DROP DESIGN</h1>
+      <SpanAleatColor className={roboto.className}/>
+    </main>
+  )
 }
