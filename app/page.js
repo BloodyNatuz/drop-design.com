c891968a9414f99a443c8d857eb1ed9061b0b5e0
Nathuz <nathuznoepupin@gmail.com>
2023-07-19 15:56:39 +0200

    💫 All page anim

--- app/page.js
+++ app/page.js
@@ -1,45 +1,35 @@
-// Components
+'use client'  
+
 import SpanAleatColor from './components/client/baselineColorAleat'
-
-// Custom Style
+import { motion } from "framer-motion"
 import main from './style/main.scss'
-
-// Custom Font
-import { Roboto } from 'next/font/google'
-import { Montserrat } from 'next/font/google'
- 
-const roboto = Roboto({
-  weight: '400',
-  subsets: ['latin'],
-  display: 'swap',
-})
-
-const montserrat = Montserrat({
-  weight: '900',
-  subsets: ['latin'],
-  display: 'swap',
-})
 
 export default function Landing() {
   return (
-    <main className='landing-page'>
-      <div className={roboto.className}>
-        <h1 className={montserrat.className}>DROP DESIGN</h1>
-        <SpanAleatColor />
+    <motion.main className='landing-page' initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 2, delay: .2 }}>
+      <div>
+        <h1>DROP DESIGN</h1>
+
+        <motion.div className='landing-page' initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 2, delay: 1 }}>
+          <SpanAleatColor />
+        </motion.div>
         
-        <a href="#">
-          <div class="body-display">
-              <div class="box-display">
-                  <div class="circle anim-pulse">
-                    <div class="circle2">
-                      <div class="circle3 anim-pulse">
-                      </div>                 
+        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: .5, delay: 2.5 }}>
+          <a href="#">
+            <div class="body-display">
+                <div class="box-display">
+                    <div class="circle anim-pulse">
+                      <div class="circle2">
+                        <div class="circle3 anim-pulse">
+                        </div>                 
+                      </div>
                     </div>
-                  </div>
-              </div>
-          </div>
-        </a>
+                </div>
+            </div>
+          </a>
+        </motion.div>
+
       </div>
-    </main>
+    </motion.main>
   )
 }
