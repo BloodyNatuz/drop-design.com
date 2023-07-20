662f9a624a3168a7d16dd0c5019964cf1000cea5
Nathuz <nathuznoepupin@gmail.com>
2023-07-20 13:59:23 +0200

    💫

---
+++ app/pages/contact/page.js
@@ -0,0 +1,14 @@
+"use client"
+
+import Navbar from '../../components/client/navbar'
+
+import main from '../../style/main.scss'
+
+export default function Home() {
+
+    return (
+      <>
+        <Navbar contact="isActive"/>
+      </>
+    )
+  }
