591a30bf2b5cc7372d73be9e42e23cd3131b4e52
Nathuz <nathuznoepupin@gmail.com>
2023-07-20 14:05:55 +0200

    🏗️  Navbar calling

--- app/page.js
+++ app/page.js
@@ -1,35 +1,38 @@
 'use client'  
 
 import SpanAleatColor from './components/client/baselineColorAleat'
-import { motion } from "framer-motion"
+import { motion, AnimatePresence } from "framer-motion"
 import main from './style/main.scss'
 
 export default function Landing() {
   return (
-    <motion.main className='landing-page' initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 2, delay: .2 }}>
-      <div>
-        <h1>DROP DESIGN</h1>
+    <AnimatePresence mode='wait'>
+      <motion.main className='landing-page' initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 2, delay: .2 }}>
+        <div>
+          <h1>DROP DESIGN</h1>
 
-        <motion.div className='landing-page' initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 2, delay: 1 }}>
-          <SpanAleatColor />
-        </motion.div>
-        
-        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: .5, delay: 2.5 }}>
-          <a href="#">
-            <div class="body-display">
-                <div class="box-display">
-                    <div class="circle anim-pulse">
-                      <div class="circle2">
-                        <div class="circle3 anim-pulse">
-                        </div>                 
+          <motion.div className='landing-page' initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ y: 10, opacity: 0 }} transition={{ duration: 2, delay: 1 }}>
+            <SpanAleatColor />
+          </motion.div>
+          
+          <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ y: 10, opacity: 0 }} transition={{ duration: .5, delay: 2.5 }}>
+            <a href="/pages/home">
+              <div class="body-display">
+                  <div class="box-display">
+                      <div class="circle anim-pulse">
+                        <div class="circle2">
+                          <div class="circle3 anim-pulse">
+                          </div>                 
+                        </div>
                       </div>
-                    </div>
-                </div>
-            </div>
-          </a>
-        </motion.div>
+                  </div>
+              </div>
+            </a>
+          </motion.div>
 
-      </div>
-    </motion.main>
+        </div>
+      </motion.main>
+    </AnimatePresence>
+
   )
 }
